e0dab80b10397cad4d0a3fd3eb5a349f015abcfd
김민건 <mine702@naver.com>
2024-01-15 13:46:46 +0900

    backend

---
+++ Backend/varium/src/main/java/b203/varium/VariumApplication.java
@@ -0,0 +1,13 @@
+package b203.varium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VariumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VariumApplication.class, args);
+	}
+
+}
