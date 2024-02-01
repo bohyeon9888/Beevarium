d0bec7fdbf2c3efa598cdabe23f7b448bf15b86e
bohyeon0808 <bohyeon9888@gmail.com>
2024-02-01 14:04:35 +0900

    Setting Security and Bcrypto Config files

---
+++ Backend/varium/src/main/java/b203/varium/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package b203.varium.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf((csrf) -> csrf.disable());
+
+        http
+                .formLogin((login) -> login.disable());
+
+        http
+                .httpBasic((basic) -> basic.disable());
+
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/", "/oauth2/**", "/login/**", "/oauth/**", "/user/join").permitAll()
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
+}
