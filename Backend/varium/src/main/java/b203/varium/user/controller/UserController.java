b1e7207ee6d5671e5b024694eb2ae224d6826fc9
bohyeon0808 <bohyeon9888@gmail.com>
2024-02-05 14:09:24 +0900

    Refactoring user controller

--- Backend/varium/src/main/java/b203/varium/user/controller/UserController.java
+++ Backend/varium/src/main/java/b203/varium/user/controller/UserController.java
@@ -3,23 +3,23 @@ package b203.varium.user.controller;
 import b203.varium.user.dto.JoinDTO;
 import b203.varium.user.dto.MyPageRespDTO;
 import b203.varium.user.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
-@ResponseBody
 @RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
     // 마이페이지 조회
     @GetMapping("/mypage")
