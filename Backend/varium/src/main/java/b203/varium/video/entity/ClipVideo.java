8f2afa458b02a0b2cbce73b009d40fff81df71f0
bohyeon0808 <bohyeon9888@gmail.com>
2024-02-05 09:29:37 +0900

    solve error

--- Backend/varium/src/main/java/b203/varium/video/entity/ClipVideo.java
+++ Backend/varium/src/main/java/b203/varium/video/entity/ClipVideo.java
@@ -6,9 +6,11 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
+
 @Entity
-@Getter @Setter
-public class git ClipVideo extends Video {
+@Getter
+@Setter
+public class ClipVideo extends Video {
     // ClipVideo specific attributes (if any)
     // `@Id` 어노테이션 제거
     // (상속받는 경우 기본 키(@Id)는 상위 클래스에서만 정의해야 함)
