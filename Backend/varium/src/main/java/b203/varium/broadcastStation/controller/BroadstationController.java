10029e59e862c8b20cee3c05af1ba5d8b205cd0c
bohyeon0808 <bohyeon9888@gmail.com>
2024-02-08 02:23:24 +0900

    update station detail

--- Backend/varium/src/main/java/b203/varium/broadcastStation/controller/BroadstationController.java
+++ Backend/varium/src/main/java/b203/varium/broadcastStation/controller/BroadstationController.java
@@ -1,18 +1,25 @@
 package b203.varium.broadcastStation.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import b203.varium.broadcastStation.dto.MyStationReqDTO;
+import b203.varium.broadcastStation.service.BroadcastStationService;
+import com.amazonaws.Response;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/station")
 public class BroadstationController {
 
+    private final BroadcastStationService broadcastStationService;
+
     // 내 방송국 조회
-    @GetMapping("/view/{stationId}")
-    public void viewMyStation(@PathVariable int stationId) {
-        
+    @PostMapping("/view")
+    public ResponseEntity<Map<String, Object>> viewMyStation(@RequestBody MyStationReqDTO reqDTO) {
+        return ResponseEntity.ok(broadcastStationService.viewMyStation(reqDTO.getStreamerId(), reqDTO.getUsername()));
     }
 
     // 방송국 내 금지 멤버 조회
