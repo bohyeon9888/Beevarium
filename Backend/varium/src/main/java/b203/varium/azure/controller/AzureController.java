ad53a13a628f8702934bbb5654c0c49062cf9c52
mine702 <mine702@naver.com>
2024-02-11 22:07:49 +0900

    develop-be

--- Backend/varium/src/main/java/b203/varium/azure/controller/AzureController.java
+++ Backend/varium/src/main/java/b203/varium/azure/controller/AzureController.java
@@ -1,10 +1,6 @@
 package b203.varium.azure.controller;
 
 import b203.varium.azure.service.AzureService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,9 +15,6 @@ public class AzureController {
     private final AzureService azureService;
 
     @MessageMapping("/change")
-    @Operation(summary = "Process text", description = "Add text to processing set and return 'Running' status", responses = {
-            @ApiResponse(responseCode = "202", description = "Accepted", content = @Content(schema = @Schema(implementation = String.class)))
-    })
     public ResponseEntity<String> processText(@RequestBody String text) {
         azureService.addTextToSet(text);
         return new ResponseEntity<>("Running", HttpStatus.ACCEPTED);
