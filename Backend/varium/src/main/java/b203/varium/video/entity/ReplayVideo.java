8672b1fa2683b49416a5d2fb14da3ce7d8f9984f
bohyeon0808 <bohyeon9888@gmail.com>
2024-02-14 15:18:16 +0900

    update video response type

--- Backend/varium/src/main/java/b203/varium/video/entity/ReplayVideo.java
+++ Backend/varium/src/main/java/b203/varium/video/entity/ReplayVideo.java
@@ -3,6 +3,7 @@ package b203.varium.video.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,6 +16,7 @@ public class ReplayVideo extends Video {
     @Column(name = "replay_video_no")
     private int replayVideoNo;
 
+    @NotNull(message = "비디오 url을 입력해야 합니다.")
     @Column(name = "replay_video_url")
     private String replayVideoUrl;
 
